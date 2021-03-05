4ee16a6de985714ec9fd14bc2de40dcc674b7801
Dishita <dishitadawda21@gmail.com>
2021-03-05 16:06:29 +0530

    first version of Hello World

--- TestProject/src/testpackage/TestClass.java
+++ TestProject/src/testpackage/TestClass.java
@@ -6,7 +6,7 @@ public class TestClass {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("How are you");
-		System.out.println("Bye");
+		System.out.println("Bye Bye");
 	}
 
 }
